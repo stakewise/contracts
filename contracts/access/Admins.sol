ac4426d56df5a4656f594ad7f038577719275d05
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2020-07-14 10:14:12 +0300

    Update Admins contract, migrate to solidity 0.6.11
    - Extract Roles.sol library from openzeppelin contracts

--- contracts/access/Admins.sol
+++ contracts/access/Admins.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity 0.5.17;
+pragma solidity 0.6.11;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/access/Roles.sol";
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "../libraries/Roles.sol";
 import "../interfaces/IAdmins.sol";
 
 /**
@@ -29,38 +29,38 @@ contract Admins is IAdmins, Initializable {
     /**
      * @dev See {IAdmins-initialize}.
      */
-    function initialize(address admin) public initializer {
-        _addAdmin(admin);
+    function initialize(address _admin) public override initializer {
+        _addAdmin(_admin);
     }
 
     /**
      * @dev See {IAdmins-isAdmin}.
      */
-    function isAdmin(address account) public view returns (bool) {
-        return admins.has(account);
+    function isAdmin(address _account) public override view returns (bool) {
+        return admins.has(_account);
     }
 
     /**
      * @dev See {IAdmins-addAdmin}.
      */
-    function addAdmin(address account) external onlyAdmin {
-        _addAdmin(account);
+    function addAdmin(address _account) external override onlyAdmin {
+        _addAdmin(_account);
     }
 
     /**
      * @dev See {IAdmins-renounceAdmin}.
      */
-    function renounceAdmin() external onlyAdmin {
+    function renounceAdmin() external override onlyAdmin {
         admins.remove(msg.sender);
         emit AdminRemoved(msg.sender);
     }
 
     /**
     * @dev Private function for assigning an admin role to the account.
-    * @param account - the account to assign an admin role to.
+    * @param _account - account to assign an admin role to.
     */
-    function _addAdmin(address account) private {
-        admins.add(account);
-        emit AdminAdded(account);
+    function _addAdmin(address _account) private {
+        admins.add(_account);
+        emit AdminAdded(_account);
     }
 }
--- contracts/interfaces/IAdmins.sol
+++ contracts/interfaces/IAdmins.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity 0.5.17;
+pragma solidity 0.6.11;
 
 /**
  * @dev Interface of the Admins contract.
@@ -8,34 +8,34 @@ pragma solidity 0.5.17;
 interface IAdmins {
     /**
     * @dev Event for tracking added admins.
-    * @param account - An address of the account which was assigned an admin role.
+    * @param account - address which was assigned an admin role.
     */
     event AdminAdded(address account);
 
     /**
     * @dev Event for tracking removed admins.
-    * @param account - An address of the account which was removed an admin role.
+    * @param account - address which was removed an admin role.
     */
     event AdminRemoved(address account);
 
     /**
     * @dev Constructor for initializing the Admins contract.
-    * @param admin - The first account to assign the admin role.
+    * @param _admin - first account to assign the admin role to.
     */
-    function initialize(address admin) external;
+    function initialize(address _admin) external;
 
     /**
     * @dev Function for checking whether an account has an admin role.
-    * @param account - The account to check.
+    * @param _account - account to check.
     */
-    function isAdmin(address account) external view returns (bool);
+    function isAdmin(address _account) external view returns (bool);
 
     /**
     * @dev Function for assigning an admin role to the account.
     * Can only be called by an account with an admin role.
-    * @param account - The account to assign an admin role to.
+    * @param _account - account to assign an admin role to.
     */
-    function addAdmin(address account) external;
+    function addAdmin(address _account) external;
 
     /**
     * @dev Function for renouncing an admin role from the account.
