e5bad843e3038c88bda33be03a374e89c0031879
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2021-03-29 00:41:36 +0300

    Implement vesting escrow and factory

---
+++ contracts/interfaces/IVestingEscrow.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity 0.7.5;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+/**
+ * @dev Interface of the VestingEscrow contract.
+ */
+interface IVestingEscrow {
+    /**
+    * @dev Event for tracking escrow stoppage.
+    * @param sender - address of the transaction sender.
+    * @param beneficiary - address of the beneficiary where all the vested tokens were transferred.
+    * @param amount - amount of vested tokens pulled.
+    */
+    event Stopped(address indexed sender, address indexed beneficiary, uint256 amount);
+
+    /**
+    * @dev Event for tracking escrow claims.
+    * @param sender - address of the transaction sender.
+    * @param beneficiary - address of the beneficiary where the tokens were transferred.
+    * @param amount - amount of unvested tokens claimed.
+    */
+    event Claimed(address indexed sender, address indexed beneficiary, uint256 amount);
+
+    /**
+    * @dev Constructor for initializing the VestingEscrow contract.
+    * @param _admin - address of the contract admin.
+    * @param _token - address of the token.
+    * @param _recipient - address of the recipient of the tokens.
+    * @param _totalAmount - amount of tokens to vest.
+    * @param _startTime - start timestamp of the vesting in seconds.
+    * @param _endTime - end timestamp of the vesting in seconds.
+    * @param _cliffLength - cliff length in seconds.
+    */
+    function initialize(
+        address _admin,
+        address _token,
+        address _recipient,
+        uint256 _totalAmount,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _cliffLength
+    ) external;
+
+    /**
+    * @dev Function for fetching the total vested tokens amount.
+    */
+    function vestedAmount() external view returns (uint256);
+
+    /**
+    * @dev Function for fetching the total unclaimed tokens amount.
+    */
+    function unclaimedAmount() external view returns (uint256);
+
+    /**
+    * @dev Function for fetching the token contract address.
+    */
+    function token() external view returns (IERC20Upgradeable);
+
+    /**
+    * @dev Function for fetching the recipient address.
+    */
+    function recipient() external view returns (address);
+
+    /**
+    * @dev Function for fetching the total vested amount.
+    */
+    function totalAmount() external view returns (uint256);
+
+    /**
+    * @dev Function for fetching the total claimed amount.
+    */
+    function claimedAmount() external view returns (uint256);
+
+    /**
+    * @dev Function for fetching vesting start time.
+    */
+    function startTime() external view returns (uint256);
+
+    /**
+    * @dev Function for fetching vesting end time.
+    */
+    function endTime() external view returns (uint256);
+
+    /**
+    * @dev Function for fetching vesting cliff length.
+    */
+    function cliffLength() external view returns (uint256);
+
+    /**
+    * @dev Function for stopping the vesting contract.
+    * Can be called only by admin. The unvested tokens will be transferred to the beneficiary address.
+    * @param beneficiary - address of the unvested tokens recipient.
+    */
+    function stop(address beneficiary) external;
+
+    /**
+    * @dev Function for claiming the vested tokens.
+    * Can be called only by the tokens recipient. The amount claimed must be vested by the time of calling.
+    * @param beneficiary - address of the tokens recipient.
+    * @param amount - amount of tokens to claim.
+    */
+    function claim(address beneficiary, uint256 amount) external;
+}
---
+++ test/vestings/VestingEscrowFactory.test.js
@@ -0,0 +1,199 @@
+const { expect } = require('chai');
+const {
+  expectRevert,
+  expectEvent,
+  ether,
+  send,
+  time,
+  BN,
+} = require('@openzeppelin/test-helpers');
+const { upgradeContracts } = require('../../deployments');
+const { contractSettings } = require('../../deployments/settings');
+const {
+  stopImpersonatingAccount,
+  impersonateAccount,
+  resetFork,
+} = require('../utils');
+
+const VestingEscrowFactory = artifacts.require('VestingEscrowFactory');
+const StakeWiseToken = artifacts.require('StakeWiseToken');
+
+contract('VestingEscrowFactory', ([recipient, anyone]) => {
+  const admin = contractSettings.admin;
+  const vestedAmount = ether('10000');
+
+  let vestingEscrowFactory, stakeWiseToken;
+
+  after(async () => stopImpersonatingAccount(admin));
+
+  beforeEach(async () => {
+    await impersonateAccount(admin);
+    await send.ether(anyone, admin, ether('5'));
+
+    let contracts = await upgradeContracts();
+    vestingEscrowFactory = await VestingEscrowFactory.at(
+      contracts.vestingEscrowFactory
+    );
+    stakeWiseToken = await StakeWiseToken.at(contracts.stakeWiseToken);
+  });
+
+  afterEach(async () => resetFork());
+
+  it('fails to deploy escrow when paused', async () => {
+    await vestingEscrowFactory.pause({ from: admin });
+    await expectRevert(
+      vestingEscrowFactory.deployEscrow(
+        stakeWiseToken.address,
+        recipient,
+        vestedAmount,
+        0,
+        time.duration.years(4),
+        time.duration.days(180),
+        {
+          from: admin,
+        }
+      ),
+      'Pausable: paused'
+    );
+  });
+
+  it('fails to deploy escrow when not admin', async () => {
+    await expectRevert(
+      vestingEscrowFactory.deployEscrow(
+        stakeWiseToken.address,
+        recipient,
+        vestedAmount,
+        0,
+        time.duration.years(4),
+        time.duration.days(180),
+        {
+          from: anyone,
+        }
+      ),
+      'OwnablePausable: access denied'
+    );
+  });
+
+  it('fails to deploy escrow with invalid cliff', async () => {
+    await expectRevert(
+      vestingEscrowFactory.deployEscrow(
+        stakeWiseToken.address,
+        recipient,
+        vestedAmount,
+        0,
+        time.duration.years(4),
+        time.duration.years(5),
+        {
+          from: admin,
+        }
+      ),
+      'VestingEscrowFactory: invalid cliff'
+    );
+  });
+
+  it('fails to deploy escrow with no token allowance', async () => {
+    await expectRevert(
+      vestingEscrowFactory.deployEscrow(
+        stakeWiseToken.address,
+        recipient,
+        vestedAmount,
+        0,
+        time.duration.years(4),
+        time.duration.days(180),
+        {
+          from: admin,
+        }
+      ),
+      'SafeMath: subtraction overflow'
+    );
+  });
+
+  it('deploys escrow for the recipient', async () => {
+    await stakeWiseToken.approve(vestingEscrowFactory.address, vestedAmount, {
+      from: admin,
+    });
+    let startTime = await time.latest();
+    let receipt = await vestingEscrowFactory.deployEscrow(
+      stakeWiseToken.address,
+      recipient,
+      vestedAmount,
+      startTime,
+      time.duration.years(4),
+      time.duration.days(180),
+      {
+        from: admin,
+      }
+    );
+    expect(await vestingEscrowFactory.balanceOf(recipient)).to.bignumber.equal(
+      vestedAmount
+    );
+    expectEvent(receipt, 'VestingEscrowCreated', {
+      admin,
+      token: stakeWiseToken.address,
+      recipient,
+      totalAmount: vestedAmount,
+      startTime,
+      endTime: startTime.add(time.duration.years(4)),
+      cliffLength: time.duration.days(180),
+    });
+  });
+
+  it('deploys multiple escrows for the recipient', async () => {
+    // deploy first escrow
+    await stakeWiseToken.approve(vestingEscrowFactory.address, vestedAmount, {
+      from: admin,
+    });
+    let startTime = await time.latest();
+    let receipt = await vestingEscrowFactory.deployEscrow(
+      stakeWiseToken.address,
+      recipient,
+      vestedAmount,
+      startTime,
+      time.duration.years(4),
+      time.duration.days(180),
+      {
+        from: admin,
+      }
+    );
+    expect(await vestingEscrowFactory.balanceOf(recipient)).to.bignumber.equal(
+      vestedAmount
+    );
+    expectEvent(receipt, 'VestingEscrowCreated', {
+      admin,
+      token: stakeWiseToken.address,
+      recipient,
+      totalAmount: vestedAmount,
+      startTime,
+      endTime: startTime.add(time.duration.years(4)),
+      cliffLength: time.duration.days(180),
+    });
+
+    // deploy second escrow
+    await stakeWiseToken.approve(vestingEscrowFactory.address, vestedAmount, {
+      from: admin,
+    });
+    receipt = await vestingEscrowFactory.deployEscrow(
+      stakeWiseToken.address,
+      recipient,
+      vestedAmount,
+      startTime,
+      time.duration.years(4),
+      time.duration.days(180),
+      {
+        from: admin,
+      }
+    );
+    expect(await vestingEscrowFactory.balanceOf(recipient)).to.bignumber.equal(
+      vestedAmount.mul(new BN(2))
+    );
+    expectEvent(receipt, 'VestingEscrowCreated', {
+      admin,
+      token: stakeWiseToken.address,
+      recipient,
+      totalAmount: vestedAmount,
+      startTime,
+      endTime: startTime.add(time.duration.years(4)),
+      cliffLength: time.duration.days(180),
+    });
+  });
+});
