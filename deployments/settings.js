7e1261afa0870c27ae6cf8d149cc13a46d5ce953
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2021-12-21 15:40:26 +0200

    Deploy goerli contracts

--- deployments/settings.js
+++ deployments/settings.js
@@ -4,9 +4,9 @@ let contracts, contractSettings;
 if (hre.hardhatArguments && hre.hardhatArguments.network === 'goerli') {
   contracts = {
     prevOracles: '0xE54486fa4eB45716c5725d7A46FDDe810B8E8914',
-    oracles: '0x4bBaA17eFd71683dCb9C769DD38E7674994FE38d',
+    oracles: '0x531b9D9cb268E88D53A87890699bbe31326A6f08',
     pool: '0x8c1EfEcFb5c4F1099AB0460b5659342943764Df7',
-    poolValidators: '0x908C06aFfda0fFDbB4578629aA8cCff372e510b6',
+    poolValidators: '0x3A2A4c01BC8595E168A90bA6F04BB8A9FEac2acb',
     poolEscrow: '0x040F15C6b5Bfc5F324eCaB5864C38D4e1EEF4218',
     stakedEthToken: '0x221D9812823DBAb0F1fB40b0D294D9875980Ac19',
     rewardEthToken: '0x826f88d423440c305D9096cC1581Ae751eFCAfB0',
