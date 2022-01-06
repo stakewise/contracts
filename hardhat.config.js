b33ffac5eb938814fc4b0fa5affa225885058ad7
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-01-06 22:50:35 +0400

    Fix tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ require('hardhat-contract-sizer');
 require('hardhat-abi-exporter');
 require('@nomiclabs/hardhat-etherscan');
 
-const BLOCK_NUMBER = 13953510;
+const BLOCK_NUMBER = 13953000;
 const OPTIMIZER_RUNS = 5000000;
 const log = (...text) => console.log(gray(...['└─> [DEBUG]'].concat(text)));
 
