b72c26e12eda2fd9cafcebc55ed834da8951b108
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-01-22 13:32:09 +0200

    Add contract deployments

--- deployments/index.js
+++ deployments/index.js
@@ -1,11 +1,262 @@
-const { contracts } = require('./settings');
+const { white, green } = require('chalk');
+const { ethers, upgrades, config } = require('hardhat');
+const { contractSettings } = require('./settings');
+
+function log(message) {
+  if (config != null && config.suppressLogs !== true) {
+    console.log(message);
+  }
+}
+
+async function deployProxy(contractName) {
+  const ImplContract = await ethers.getContractFactory(contractName);
+  const proxy = await upgrades.deployProxy(ImplContract, [], {
+    initializer: false,
+    kind: 'transparent',
+  });
+  await proxy.deployed();
+  return proxy.address;
+}
+
+async function deployPoolEscrow() {
+  const PoolEscrow = await ethers.getContractFactory('PoolEscrow');
+  const escrow = await PoolEscrow.deploy(contractSettings.admin);
+
+  await escrow.deployed();
+  return escrow.address;
+}
+
+async function deployRoles() {
+  const Roles = await ethers.getContractFactory('Roles');
+  const proxy = await upgrades.deployProxy(Roles, [contractSettings.admin], {
+    kind: 'transparent',
+  });
+  await proxy.deployed();
+  return proxy.address;
+}
+
+async function deployContractChecker() {
+  const ContractChecker = await ethers.getContractFactory('ContractChecker');
+  const contractChecker = await ContractChecker.deploy();
+
+  await contractChecker.deployed();
+  return contractChecker.address;
+}
+
+async function initializePool(
+  poolAddress,
+  poolEscrowAddress,
+  stakedEthTokenAddress,
+  validatorsAddress,
+  oraclesAddress
+) {
+  const Pool = await ethers.getContractFactory('Pool');
+  const withdrawalCreds = ethers.utils.hexConcat([
+    '0x01',
+    '0x' + '00'.repeat(11),
+    poolEscrowAddress,
+  ]);
+
+  let pool = Pool.attach(poolAddress);
+
+  // call initialize
+  return pool.initialize(
+    contractSettings.admin,
+    withdrawalCreds,
+    contractSettings.validatorRegistration,
+    stakedEthTokenAddress,
+    validatorsAddress,
+    oraclesAddress,
+    contractSettings.minActivatingDeposit,
+    contractSettings.pendingValidatorsLimit
+  );
+}
+
+async function initializePoolValidators(
+  poolValidatorsAddress,
+  poolAddress,
+  oraclesAddress
+) {
+  const PoolValidators = await ethers.getContractFactory('PoolValidators');
+  let poolValidators = PoolValidators.attach(poolValidatorsAddress);
+
+  // call initialize
+  return poolValidators.initialize(
+    contractSettings.admin,
+    poolAddress,
+    oraclesAddress
+  );
+}
+
+async function initializeMerkleDistributor(
+  merkleDistributorAddress,
+  rewardEthTokenAddress,
+  oraclesAddress
+) {
+  const MerkleDistributor = await ethers.getContractFactory(
+    'MerkleDistributor'
+  );
+
+  let merkleDistributor = MerkleDistributor.attach(merkleDistributorAddress);
+
+  // call initialize
+  return merkleDistributor.initialize(
+    contractSettings.admin,
+    rewardEthTokenAddress,
+    oraclesAddress
+  );
+}
+
+async function initializeRewardEthToken(
+  rewardEthTokenAddress,
+  stakedEthTokenAddress,
+  oraclesAddress,
+  merkleDistributorAddress
+) {
+  const RewardEthToken = await ethers.getContractFactory('RewardEthToken');
+  let rewardEthToken = RewardEthToken.attach(rewardEthTokenAddress);
+
+  // call initialize
+  return rewardEthToken.initialize(
+    contractSettings.admin,
+    stakedEthTokenAddress,
+    oraclesAddress,
+    contractSettings.protocolFeeRecipient,
+    contractSettings.protocolFee,
+    merkleDistributorAddress
+  );
+}
+
+async function initializeStakedEthToken(
+  stakedEthTokenAddress,
+  poolAddress,
+  rewardEthTokenAddress
+) {
+  const StakedEthToken = await ethers.getContractFactory('StakedEthToken');
+  let stakedEthToken = StakedEthToken.attach(stakedEthTokenAddress);
+
+  // call initialize
+  return stakedEthToken.initialize(
+    contractSettings.admin,
+    poolAddress,
+    rewardEthTokenAddress
+  );
+}
+
+async function initializeOracles(
+  oraclesAddress,
+  rewardEthTokenAddress,
+  poolAddress,
+  poolValidatorsAddress,
+  merkleDistributorAddress
+) {
+  const Oracles = await ethers.getContractFactory('Oracles');
+  let oracles = Oracles.attach(oraclesAddress);
+
+  // call initialize
+  return oracles.initialize(
+    contractSettings.admin,
+    rewardEthTokenAddress,
+    poolAddress,
+    poolValidatorsAddress,
+    merkleDistributorAddress
+  );
+}
 
 async function deployContracts() {
-  return contracts;
+  const poolEscrowAddress = await deployPoolEscrow();
+  log(white(`Deployed Pool Escrow contract: ${green(poolEscrowAddress)}`));
+
+  const contractCheckerAddress = await deployContractChecker();
+  log(
+    white(`Deployed ContractChecker contract: ${green(contractCheckerAddress)}`)
+  );
+
+  const rolesAddress = await deployRoles();
+  log(`Deployed Roles contract: ${green(rolesAddress)}`);
+
+  const poolAddress = await deployProxy('Pool');
+  log(`Deployed Pool contract: ${green(poolAddress)}`);
+
+  const poolValidatorsAddress = await deployProxy('PoolValidators');
+  log(`Deployed PoolValidators contract: ${green(poolValidatorsAddress)}`);
+
+  const rewardEthTokenAddress = await deployProxy('RewardEthToken');
+  log(`Deployed RewardEthToken contract: ${green(rewardEthTokenAddress)}`);
+
+  const stakedEthTokenAddress = await deployProxy('StakedEthToken');
+  log(`Deployed StakedEthToken contract: ${green(stakedEthTokenAddress)}`);
+
+  const merkleDistributorAddress = await deployProxy('MerkleDistributor');
+  log(
+    `Deployed MerkleDistributor contract: ${green(merkleDistributorAddress)}`
+  );
+
+  const oraclesAddress = await deployProxy('Oracles');
+  log(`Deployed Oracles contract: ${green(oraclesAddress)}`);
+
+  await initializeMerkleDistributor(
+    merkleDistributorAddress,
+    rewardEthTokenAddress,
+    oraclesAddress
+  );
+  log(white('Initialized MerkleDistributor contract'));
+
+  await initializePool(
+    poolAddress,
+    poolEscrowAddress,
+    stakedEthTokenAddress,
+    poolValidatorsAddress,
+    oraclesAddress
+  );
+  log(white('Initialized Pool contract'));
+
+  await initializePoolValidators(
+    poolValidatorsAddress,
+    poolAddress,
+    oraclesAddress
+  );
+  log(white('Initialized PoolValidators contract'));
+
+  await initializeRewardEthToken(
+    rewardEthTokenAddress,
+    stakedEthTokenAddress,
+    oraclesAddress,
+    merkleDistributorAddress
+  );
+  log(white('Initialized RewardEthToken contract'));
+
+  await initializeStakedEthToken(
+    stakedEthTokenAddress,
+    poolAddress,
+    rewardEthTokenAddress
+  );
+  log(white('Initialized StakedEthToken contract'));
+
+  await initializeOracles(
+    oraclesAddress,
+    rewardEthTokenAddress,
+    poolAddress,
+    poolValidatorsAddress,
+    merkleDistributorAddress
+  );
+  log(white('Initialized Oracles contract'));
+
+  return {
+    oracles: oraclesAddress,
+    pool: poolAddress,
+    poolValidators: poolValidatorsAddress,
+    poolEscrow: poolEscrowAddress,
+    stakedEthToken: stakedEthTokenAddress,
+    rewardEthToken: rewardEthTokenAddress,
+    merkleDistributor: merkleDistributorAddress,
+    roles: rolesAddress,
+    contractChecker: contractCheckerAddress,
+  };
 }
 
 async function upgradeContracts() {
-  return contracts;
+  return deployContracts();
 }
 
 module.exports = {
--- deployments/settings.js
+++ deployments/settings.js
@@ -1,51 +1,26 @@
 const hre = require('hardhat');
 
-let contracts, contractSettings;
+let contractSettings;
 if (hre.hardhatArguments && hre.hardhatArguments.network === 'goerli') {
-  contracts = {
-    oracles: '0x531b9D9cb268E88D53A87890699bbe31326A6f08',
-    pool: '0x8c1EfEcFb5c4F1099AB0460b5659342943764Df7',
-    poolValidators: '0x3A2A4c01BC8595E168A90bA6F04BB8A9FEac2acb',
-    poolEscrow: '0x040F15C6b5Bfc5F324eCaB5864C38D4e1EEF4218',
-    stakedEthToken: '0x221D9812823DBAb0F1fB40b0D294D9875980Ac19',
-    rewardEthToken: '0x826f88d423440c305D9096cC1581Ae751eFCAfB0',
-    stakeWiseToken: '0x0e2497aACec2755d831E4AFDEA25B4ef1B823855',
-    vestingEscrowFactory: '0xbA91cdD484893c1f8F75DB55733ccaDcd0fE5f59',
-    merkleDrop: '0xFc3513E92799F0169e5f14F354d0097E4b790498',
-    merkleDistributor: '0x6Ef0172b79131C66c7012Db3545D637B116feb12',
-    roles: '0x3ae8a774CFBBE305520A4a3Be3A480701B66aFba',
-    contractChecker: '0x85ee326f839Bc430655A3fad447837072ef52C2F',
-    proxyAdmin: '0xbba3f4dDD4F705aD2028ee2da64fF3166bDe8cA8',
-  };
-
   contractSettings = {
     admin: '0x1867c96601bc5fE24F685d112314B8F3Fe228D5A',
-    vestingEscrow: '0x4CDAe3f1Eaa84b88fFc97627Ef1c77F762794287',
+    validatorRegistration: '0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b',
+    protocolFeeRecipient: '0x1867c96601bc5fE24F685d112314B8F3Fe228D5A',
+    minActivatingDeposit: '32000000000000000000',
+    pendingValidatorsLimit: '500',
+    protocolFee: '1000',
   };
 } else {
-  contracts = {
-    oracles: '0x8a887282E67ff41d36C0b7537eAB035291461AcD',
-    pool: '0xC874b064f465bdD6411D45734b56fac750Cda29A',
-    poolValidators: '0x002932e11E95DC84C17ed5f94a0439645D8a97BC',
-    poolEscrow: '0x2296e122c1a20Fca3CAc3371357BdAd3be0dF079',
-    stakedEthToken: '0xFe2e637202056d30016725477c5da089Ab0A043A',
-    rewardEthToken: '0x20BC832ca081b91433ff6c17f85701B6e92486c5',
-    stakeWiseToken: '0x48C3399719B582dD63eB5AADf12A40B4C3f52FA2',
-    vestingEscrowFactory: '0x7B910cc3D4B42FEFF056218bD56d7700E4ea7dD5',
-    merkleDrop: '0x2AAB6822a1a9f982fd7b0Fe35A5A5b6148eCf4d5',
-    merkleDistributor: '0xA3F21010e8b9a3930996C8849Df38f9Ca3647c20',
-    roles: '0xC486c10e3611565F5b38b50ad68277b11C889623',
-    contractChecker: '0xFC1fC7257AEA7C7c08A498594DCA97CE5A72fdCB',
-    proxyAdmin: '0x3EB0175dcD67d3AB139aA03165e24AA2188A4C22',
-  };
-
   contractSettings = {
     admin: '0x144a98cb1CdBb23610501fE6108858D9B7D24934',
-    vestingEscrow: '0x1E6d872CE26C8711e7D47b8E0C47aB91d95a6dF3',
+    validatorRegistration: '0x00000000219ab540356cBB839Cbe05303d7705Fa',
+    protocolFeeRecipient: '0x1867c96601bc5fE24F685d112314B8F3Fe228D5A',
+    minActivatingDeposit: '32000000000000000000',
+    pendingValidatorsLimit: '500',
+    protocolFee: '1000',
   };
 }
 
 module.exports = {
   contractSettings,
-  contracts,
 };
