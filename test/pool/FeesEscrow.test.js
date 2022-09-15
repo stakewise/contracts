28b27f7e43077303fda5cdbd99684f8883bffbf9
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-09-15 16:31:14 +0300

    Update FeesEscrow test

--- test/pool/FeesEscrow.test.js
+++ test/pool/FeesEscrow.test.js
@@ -46,16 +46,11 @@ contract('FeesEscrow', (accounts) => {
     await impersonateAccount(contracts.oracles);
     const oraclesSigner = await ethers.getSigner(contracts.oracles);
     const feesEscrowBalance = await balance.current(contracts.feesEscrow);
-    const feesAmount = ethers.utils.parseEther('1');
+    const feesAmount = ether('1');
 
     // Ensure zero balances before miner's reward distribution to FeesEscrow contract
     const poolBalanceBefore = await balance.current(pool.address);
     expect(poolBalanceBefore.toString()).to.be.bignumber.equal(new BN('0'));
-
-    const feesEscrowBalanceBefore = await balance.current(feesEscrow.address);
-    expect(feesEscrowBalanceBefore.toString()).to.be.bignumber.equal(
-      new BN('0')
-    );
 
     // Send fees from "validator" to FeesEscrow contract
     await send.ether(sender, feesEscrow.address, feesAmount.toString());
@@ -66,15 +61,15 @@ contract('FeesEscrow', (accounts) => {
       '0x100000000000000000',
     ]);
 
-    const newTotalRewards = ethers.utils.parseEther('100000');
+    const newTotalRewards = ether('100000');
     await rewardEthToken
       .connect(oraclesSigner)
-      .updateTotalRewards(newTotalRewards);
+      .updateTotalRewards(newTotalRewards.toString());
 
     // Ensure all fees transferred from FeesEscrow contract to Pool contract
     const poolBalanceAfter = await balance.current(pool.address);
     expect(poolBalanceAfter.toString()).to.be.bignumber.equal(
-      feesEscrowBalance.add(feesAmount).toString()
+      poolBalanceBefore.add(feesEscrowBalance).add(feesAmount).toString()
     );
 
     const feesEscrowBalanceAfterTransfer = await balance.current(
@@ -87,12 +82,7 @@ contract('FeesEscrow', (accounts) => {
 
   it('transferToPool from invalid caller', async () => {
     // Send fees from "validator" to FeesEscrow contract
-    await send.ether(
-      sender,
-      feesEscrow.address,
-      ethers.utils.parseEther('1').toString()
-    );
-
+    await send.ether(sender, feesEscrow.address, ether('1').toString());
     await expectRevert(
       feesEscrow.transferToPool(),
       'FeesEscrow: invalid caller'
