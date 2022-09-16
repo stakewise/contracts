dd38012e4e0196cdbc255d206c1bbf1143f6a6db
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-09-16 11:46:25 +0300

    Upgrade RewardEthToken proxy

--- deployments/index.js
+++ deployments/index.js
@@ -1,35 +1,10 @@
-const { white } = require('chalk');
-const { ethers, upgrades, config } = require('hardhat');
-const { contracts, contractSettings } = require('./settings');
-
-function log(message) {
-  if (config != null && config.suppressLogs !== true) {
-    console.log(message);
-  }
-}
-
-async function upgradeRewardEthToken() {
-  const signer = await ethers.provider.getSigner(contractSettings.admin);
-  const RewardEthToken = await ethers.getContractFactory(
-    'RewardEthToken',
-    signer
-  );
-  // upgrade RewardEthToken to new implementation
-  const proxy = await upgrades.upgradeProxy(
-    contracts.rewardEthToken,
-    RewardEthToken
-  );
-  await proxy.deployed();
-}
+const { contracts } = require('./settings');
 
 async function deployContracts() {
   return contracts;
 }
 
 async function upgradeContracts() {
-  await deployContracts();
-  await upgradeRewardEthToken();
-  log(white('Upgraded RewardEthToken contract'));
   return contracts;
 }
 
